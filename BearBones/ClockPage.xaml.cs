caf369cd50b32be50b8f86ee2d4c8858bf6e6d60
Paul Mace <paul.mace@gmail.com>
2014-12-20 14:41:44 -0800

    Initial check-in of module BearBones

---
+++ BearBones/ClockPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BearBones
+{
+	public partial class ClockPage
+	{
+		public ClockPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
---
+++ BearBones/HomePageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BearBones
+{
+	class HomePageViewModel
+	{
+		public HomePageViewModel(Type pageType,
+			Action<Type> gotoExecute, 
+			Action<String> browseExecute)
+		{
+			this.PageType = pageType;
+			this.PageName = pageType.Name;
+			this.GoToCommand = new Command<Type>(gotoExecute);
+			this.BrowseCommand = new Command<String>(browseExecute);
+		}
+
+		public Type PageType { private set; get; }
+
+		public string PageName { private set; get; }
+
+		public ICommand GoToCommand { private set; get; }
+
+		public ICommand BrowseCommand { private set; get; }
+	}
+}
+
