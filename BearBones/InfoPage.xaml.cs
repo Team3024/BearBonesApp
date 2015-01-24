ebddeae52c64b196be44892cd6fcf128271d5dcf
Joseph Yaconelli <josephyaconelli@gmail.com>
2015-01-24 11:32:31 -0800

    added background logo to info page.

--- BearBones/InfoPage.xaml.cs
+++ BearBones/InfoPage.xaml.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 
+
 namespace BearBones
 {	
 	public partial class InfoPage : ContentPage
 	{	
+	
+
 		public InfoPage (string teamNumber)
 		{
+
 			InitializeComponent ();
+
 		}
 
 		void OnDoneClicked (object sender, EventArgs e)
 		{
 			// leave this page
 			Navigation.PopModalAsync ();
+		
+		}
+
+		//test for replacing value of Label
+		void ChangeValue(object sender, EventArgs e)
+		{
+			field1Entry.Text = "lots and lots";
 		}
 	}
 }
