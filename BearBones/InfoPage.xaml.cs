2d61ded6c6b9f96ac3a4648235d2be3dcc498bc0
Joseph Yaconelli <josephyaconelli@gmail.com>
2015-02-02 10:03:22 -0800

    Displays the first report on the info page. (trying to put info into arrays. Doesn't work yet. Code commented out)

--- BearBones/InfoPage.xaml.cs
+++ BearBones/InfoPage.xaml.cs
@@ -9,11 +9,17 @@ namespace BearBones
 	public partial class InfoPage : CarouselPage
 	{	
 	
+		string[] scores;
+		string[] drives;
+		string[] scouts;
 
-		public InfoPage (string teamNumber)
+		public InfoPage (HomePageViewModel hpvm)
 		{
 
 			InitializeComponent ();
+			getReports(hpvm.teamNumber);
+
+			title1.Text = hpvm.teamName + " " + hpvm.teamNumber + ": Rprt 1";
 
 
 		}
@@ -33,13 +39,44 @@ namespace BearBones
 
 			Rest rest = new Rest();
 
-			Task <ObservableCollection<InfoPageViewModel>> list = rest.SendAndReceiveJsonRequest ("http://71.92.131.203/db/data/cypher/","MATCH (a:Team) RETURN a LIMIT 25");
-
+			//Task <ObservableCollection<InfoPageViewModel>> list = rest.SendAndReceiveJsonRequest ("http://71.92.131.203/db/data/cypher/","MATCH (a:Team) RETURN a LIMIT 25");
+			/*
 			try {
 				field1Entry.Text = int.Parse (field1Entry.Text) > 50 ? "impressive" : "could\'ve done better";
 			} catch {
 				field1Entry.Text = "I\'m sorry. Something didn\'t work.";
 			}
+			*/
 		}
+
+		async void getReports(int tNum)
+		{
+			Rest rest = new Rest ();
+			Task <ObservableCollection<InfoPageViewModel>> list =  rest.getReports (tNum);//SendAndReceiveJsonRequest ();
+			var reports = await list;
+			var count = 0;
+			foreach (InfoPageViewModel ip in reports) 
+			{
+				var p=ip;
+				scoreEntry.Text = Convert.ToString (ip.score);
+				driveEntry.Text = Convert.ToString (ip.drive);
+				scoutEntry.Text = Convert.ToString (ip.scout);
+
+				//scores [count] = Convert.ToString (ip.score);
+				//drives [count] = Convert.ToString (ip.drive);
+				//scouts [count] = Convert.ToString (ip.scout);
+
+				//NEED TO FIGURE THIS OUT
+			
+				//count++;
+
+			}
+
+	
+		}
+
+
+
+
 	}
 }
