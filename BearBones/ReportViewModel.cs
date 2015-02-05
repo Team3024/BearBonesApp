54347d65a1df6fa474e0dadf164141909ee34ed0
MacOwner <>
2015-02-04 17:22:13 -0800

    * MatchReportPage.xaml.cs: Added controls to Scout Report page added properties to ReportViewModel need to process values on ScoutReport Done and need to modify REST   call to use store and retrieve those values
    * REST.cs:
    * ReportViewModel.cs:
    * ScoutReportPage.xaml:
    * ScoutReportPage.xaml.cs:

--- BearBones/ReportViewModel.cs
+++ BearBones/ReportViewModel.cs
@@ -28,9 +28,29 @@ namespace BearBones
 
 		public int matchNumber {  set; get; }
 
-		public string driveType {  set; get; }
+		public string driveType {  set; get; }		//swerve,mecanum,tank,other
+		public string buildQuality {  set; get; }	//junk,low,ok,high,superb
+		public string lastYearFinish {  set; get; }	//prelim,regional,world
+		public string autoCapability {  set; get; }	//never moved,in-zone, 1 container, 1 tote, tote stack
+		public string maxStack {  set; get; }		//1,2,3,4,5,6
+		public bool breakdown {  set; get; }
 
-		public int score {  set; get; }
+		public bool noodleInContainer {  set; get; }
+		public bool noodleCleanup {  set; get; }
+		public bool noodleBonus {  set; get; }
+
+		public bool containerOnStack {  set; get; }
+		public bool coopStack {  set; get; }
+		public bool grabContainer {  set; get; }
+		public bool grabContainerFromStep {  set; get; }
+
+		public bool grabTotes {  set; get; }
+		public bool grabTotesOffStep {  set; get; }
+		public bool rebuildStack {  set; get; }
+		public string teamRating {  set; get; }	//bad,poor,ok,good,superb
+		public string notes {  set; get; }
+		//add photo
+		public int pointsScored {  set; get; }	//0,5,10,20,30,40,50-200
 	}
 }
 
--- BearBones/ScoutReportPage.xaml.cs
+++ BearBones/ScoutReportPage.xaml.cs
@@ -1,7 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+/*
+SCOUT REPORT
 
+drive picker--swerve,mecanum,tank,other
+build quality--junk,low,ok,high,superb
+last year--prelim,regional,world
+noodle in cont: yes/no
+Cont on stack yes/no
+noodle cleanup
+noodle bonus
+add photo
+
+MATCH REPORT
+
+Breakdown: yes/no
+noodle in cont: yes/no
+Cont on stack yes/no
+Points scored: 0,5,10,20,30,40,50-200
+coop stack: yes/no
+auto: never moved,in-zone, 1 container, 1 tote, tote stack
+grab containers off step: yes/no
+grab totes off step: Yes/no
+rebuild stacks.
+noodle cleanup
+noodle bonus
+Team Rating: bad,poor,ok,good,superb
+Notes:?
+
+Save Report
+
+
+
+
+*/
 namespace BearBones
 {	
 	public partial class ScoutReportPage : ContentPage
@@ -21,7 +54,7 @@ namespace BearBones
 			else
 				model.teamNumber = 0;
 			Random rand = new Random ();
-			model.score = rand.Next();
+			model.pointsScored = rand.Next();
 			model.driveType = "mecanum";
 			model.matchNumber = rand.Next();
 			model.reportType = "scout";
