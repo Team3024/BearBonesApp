c71e02041d4df916ea65d462778bf98524bc3edf
MacOwner <>
2015-02-05 18:21:55 -0800

    * InfoPageViewModel.cs: ViewModel has comments with snippets of   graph code I will add support code to IOS and Android app and main cs files
    * TeamCell.cs:

--- BearBones/InfoPageViewModel.cs
+++ BearBones/InfoPageViewModel.cs
@@ -15,4 +15,59 @@ namespace BearBones
 		}
 	}
 }
+/*
+ * 
+ * Grab nuget Xlabs.Forms 2.0
+using XLabs.Forms.Charting;
+using XLabs.Forms.Charting.Controls;
 
+...code snippet
+
+						Series firstBarSeries = new Series
+						{
+							Color = Color.Red,
+							Type = ChartType.Bar
+						};
+						firstBarSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 25});
+						firstBarSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 40});
+						firstBarSeries.Points.Add(new DataPoint() { Label = "March", Value = 45});
+
+						Series secondBarSeries = new Series
+						{
+							Color = Color.Blue,
+							Type = ChartType.Bar
+						};
+						secondBarSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 30 });
+						secondBarSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 35 });
+						secondBarSeries.Points.Add(new DataPoint() { Label = "March", Value = 40 });
+
+						Series lineSeries = new Series
+						{
+							Color = Color.Green,
+							Type = ChartType.Line
+						};
+						lineSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 27.5 });
+						lineSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 37.5 });
+						lineSeries.Points.Add(new DataPoint() { Label = "March", Value = 42.5 });
+						
+
+						Chart chart = new Chart()
+						{
+							Color = Color.White,
+							WidthRequest = MainPage.ScreenWidth-10,
+							HeightRequest = 100,
+							Spacing = 10
+						};
+						chart.Series.Add(firstBarSeries);
+						chart.Series.Add(secondBarSeries);
+						chart.Series.Add(lineSeries);
+						StackLayout stack = new StackLayout ();
+						
+						Label lbl = new Label {
+							Text = temp.PageName
+						};
+
+						stack.Children.Add(lbl);
+						stack.Children.Add(chart);
+						this.View = stack;
+ * */
--- BearBones/TeamCell.cs
+++ BearBones/TeamCell.cs
@@ -11,60 +11,6 @@ namespace BearBones
 		{
 			var temp = BindingContext as HomePageViewModel;
 
-
-			// Populating the ViewCell is now deferred to OnBindingContextChanged () below
-			// This lets us customize things like size and widget content for each cell
-
-			/*
-			Label title = new Label
-			{
-				HorizontalOptions = LayoutOptions.StartAndExpand,
-				VerticalOptions = LayoutOptions.Center
-			};
-			title.SetBinding (Label.TextProperty, new Binding("PageName"));
-
-
-			Button infoBtn = new Button
-			{
-				Text="Info",
-				//Command="InfoCommand",
-				CommandParameter="teamNumber"
-				
-			};
-
-			//infoBtn.SetBinding (Button.Command, new Binding("InfoCommand"));
-			//infoBtn.SetBinding (Button.CommandParameter,new Binding("teamNumber"));
-
-			Button scoutBtn = new Button
-			{
-				Text="New Scout Report",
-				//Command="ScoutReportCommand",
-				CommandParameter="teamNumber"
-			};
-
-			//scoutBtn.SetBinding (Button.Command, "ScoutReportCommand");
-			//scoutBtn.SetBinding (Button.CommandParameter, "teamNumber");
-
-			Button matchBtn = new Button
-			{
-				Text="New Match Report",
-				//Command="ScoutReportCommand",
-				CommandParameter="teamNumber"
-			};
-			//matchBtn.SetBinding (Button.Command, "MatchReportCommand");
-			//matchBtn.SetBinding (Button.CommandParameter, "teamNumber");
-
-			var s = new StackLayout
-			{
-				Orientation=StackOrientation.Horizontal,
-				Padding = new Thickness(5,0,5,0)
-			};
-			s.Children.Add (title);
-			s.Children.Add (infoBtn);
-			s.Children.Add (scoutBtn);
-			s.Children.Add (matchBtn);
-			this.View = s;
-			*/
 		}
 
 
@@ -85,81 +31,18 @@ namespace BearBones
 			};
 
 
-
 			title.SetBinding (Label.TextProperty, new Binding("PageName"));
-			/*
 
-			Button infoBtn = new Button
-			{
-				Text="Info",
-				HorizontalOptions = LayoutOptions.StartAndExpand,
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				//Command="InfoCommand",
-				CommandParameter="teamNumber"
-			};
-			infoBtn.SetBinding<HomePageViewModel> (Button.CommandProperty,f => f.InfoCommand);
-			infoBtn.SetBinding<HomePageViewModel> (Button.CommandParameterProperty,f => f.teamNumber);
-
-			Button scoutBtn = new Button
-			{
-				Text="+ScoutReport",
-				HorizontalOptions = LayoutOptions.StartAndExpand,
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				//Command="ScoutReportCommand",
-				CommandParameter="teamNumber"
-			};
-
-			scoutBtn.SetBinding<HomePageViewModel> (Button.CommandProperty,f => f.ScoutReportCommand);
-			infoBtn.SetBinding<HomePageViewModel> (Button.CommandParameterProperty,f => f.teamNumber);
-
-			Button matchBtn = new Button
-			{
-				Text="+MatchReport",
-				//Command="ScoutReportCommand",
-				CommandParameter="teamNumber"
-			};
-			matchBtn.SetBinding<HomePageViewModel> (Button.CommandProperty,f => f.MatchReportCommand);
-			infoBtn.SetBinding<HomePageViewModel> (Button.CommandParameterProperty,f => f.teamNumber);
-*/
 			var s = new StackLayout
 			{
 				Orientation=StackOrientation.Horizontal,
 				Padding = new Thickness(5,0,5,0)
 			};
 			s.Children.Add (title);
-			//s.Children.Add (infoBtn);
-			//s.Children.Add (scoutBtn);
-			//s.Children.Add (matchBtn);
 			this.View = s;
-
-
 		}
 
 	}
 
 }
 
-/*
-          <? <ViewCell> 
-          <StackLayout Orientation="Horizontal"
-                       Padding="5, 0"
-                       >
-            <Label Text="{Binding PageName}"
-                   HorizontalOptions="StartAndExpand"
-                   VerticalOptions="Center" />
-
-            <Button Text="Info"
-                    Command="{Binding InfoCommand}"
-                    CommandParameter="{Binding teamNumber}" />
-
-            <Button Text="New Scout Report"
-                    Command="{Binding ScoutReportCommand}"
-                    CommandParameter="{Binding teamNumber}" />
-            
-            <Button Text="New Match Report"
-                    Command="{Binding MatchReportCommand}"
-                    CommandParameter="{Binding teamNumber}" />
-
-          </StackLayout>
-        </ViewCell> ?>
- */
