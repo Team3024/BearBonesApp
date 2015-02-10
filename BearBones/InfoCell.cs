2b97f00749d94c57051be0c02f5d95df459f6873
Joseph Yaconelli <>
2015-02-09 16:37:05 -0800

    Added visualization of full reports.

--- BearBones/InfoCell.cs
+++ BearBones/InfoCell.cs
@@ -3,8 +3,11 @@ using Xamarin.Forms;
 
 namespace BearBones
 {
-	public class InfoCell
+	public class InfoCell : ContentPage
 	{
+
+
+
 		public InfoCell ()
 		{
 
@@ -13,13 +16,130 @@ namespace BearBones
 		}
 
 
-		public ContentPage CreatePage(string teamScore, string teamDrive, string scoutName){
+		public ContentPage CreatePage(string teamScore,
+			string teamDrive,
+			string scoutName,
+			string autoCapabilities,
+			bool brokeDown,
+			string buildQuality,
+			bool grabsContainer,
+			bool grabsContainerOffStep,
+			bool grabsTote,
+			bool grabsToteOffStep,
+			string lastYearFinish,
+			string matchNumber,
+			string maxStack,
+			bool noodleBonus,
+			bool noodleCleaup,
+			bool noodleInContainer,
+			string notes,
+			bool rebuildStack,
+			string reportType,
+			bool setsContainerOnStack,
+			bool stacksTotes,
+			string teamName,
+			int teamNumber,
+			string teamQuality,
+			bool yellowCoopStack,
+			int pageNumber)
+		{
+
+
+
+			string brokeDownText = (brokeDown) ? "broke down" : "didn\'t break down";
+			string grabsContainerText = (grabsContainer) ? "grabs containers" : "can\'t grab containers";
+			string grabsContainerOffStepText = (grabsContainerOffStep) ? "grabs containers off step" : "can\'t grab containers off step";
+			string grabsToteText = (grabsTote) ? "grabs totes" : "can\'t grab totes";
+			string grabsToteOffStepText = (grabsToteOffStep) ? "grabs tote off step" : "can\'t grab tote off step";
+			string noodleBonusText = (noodleBonus) ? "executes noodle bonus" : "can\'t execute noodle bonus";
+			string noodleCleanupText = (noodleCleaup) ? "cleans up noodles" : "can\'t clean up noodles";
+			string noodleInContainerText = (noodleInContainer) ? "puts noodle in container" : "can\'t put noodle in container";
+			string rebuildStackText = (rebuildStack) ? "rebuilds stacks" : "can\'t rebuild stacks";
+			string setsContainerOnStackText = (setsContainerOnStack) ? "sets containers on stacks" : "can\'t set containers on stacks";
+			string stacksTotesText = (stacksTotes) ? "stackes totes" : "can\'t stack totes";
+			string yellowCoopStackText = (yellowCoopStack) ? "execute co-op stack" : "can\'t execute co-op stack";
+
+
 			var report = new ContentPage {
-				Content = new StackLayout{
-					Children = {
-						new Label {Text = "Score: " + teamScore},
-						new Label {Text = "Drive: " + Convert.ToString(teamDrive)},
-						new Label {Text = "Scout: " + Convert.ToString(scoutName)},
+
+				Content = new ScrollView{ 
+					Content = new StackLayout{
+						BackgroundColor= new Color(0,0,0,0),
+						Padding=5,
+						Children = {
+
+							new Label {Text = "",
+								HeightRequest=12},
+							new Label {Text = "Score: " + teamScore,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Drive: " + teamDrive,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Scout: " + scoutName,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Auto Capabilities: " + autoCapabilities,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text="Robot:"},
+							new Label {Text = "\t\t\t\t" + brokeDownText, Style = Device.Styles.BodyStyle,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + grabsContainerText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + grabsContainerOffStepText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + grabsToteText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + grabsToteOffStepText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + noodleBonusText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + noodleCleanupText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + noodleInContainerText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + rebuildStackText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + setsContainerOnStackText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + stacksTotesText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "\t\t\t\t" + yellowCoopStackText,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Build Quality: " + buildQuality,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Team Quality: " + teamQuality,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Max Stack: " + maxStack,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Last Year Finish: " + lastYearFinish,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = "Match Number: " + matchNumber,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {HeightRequest=20},
+							new Label {Text = "Notes: " + notes,
+								//FontSize=25,
+								FontAttributes= FontAttributes.Bold},
+							new Label {Text = pageNumber.ToString(), AnchorY = HomePage.ScreenHeight - 20}
+						}
 
 					}
 				}
@@ -28,12 +148,6 @@ namespace BearBones
 			return report;
 		}
 
-		//	protected override void OnBindingContextChanged ()
-		//	{
-
-
-
-		//	}
 	}
 }
 
--- BearBones/InfoPage.xaml.cs
+++ BearBones/InfoPage.xaml.cs
@@ -3,6 +3,8 @@ using Xamarin.Forms;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using XLabs.Forms.Charting;
+using XLabs.Forms.Charting.Controls;
 
 namespace BearBones
 {	
@@ -11,6 +13,32 @@ namespace BearBones
 
 	public partial class InfoPage : CarouselPage
 	{	
+
+		public List<string> pointsScoreds = new List<string> ();
+		public List<string> driveTypes = new List<string> ();
+		public List<string> scoutNames = new List<string> ();
+		public List<string> autoCapabilities = new List<string> ();
+		public List<bool> brokeDowns = new List<bool> ();
+		public List<string> buildQualities = new List<string> ();
+		public List<bool> grabsContainers = new List<bool> ();
+		public List<bool> grabsContainerOffSteps = new List<bool> ();
+		public List<bool> grabsTotes = new List<bool> ();
+		public List<bool> grabsToteOffSteps = new List<bool> ();
+		public List<string> lastYearFinishes = new List<string> ();
+		public List<string> matchNumbers = new List<string> ();
+		public List<string> maxStacks = new List<string> ();
+		public List<bool> noodleBonuses = new List<bool> ();
+		public List<bool> noodleCleanups = new List<bool> ();
+		public List<bool> noodleInContainers = new List<bool> ();
+		public List<string> noteses = new List<string> ();
+		public List<bool> rebuildsStacks = new List<bool> ();
+		public List<string> reportTypes = new List<string> ();
+		public List<bool> setsContainerOnStacks = new List<bool> ();
+		public List<bool> stacksToteses = new List<bool> ();
+		public List<string> teamNames = new List<string> ();
+		public List<int> teamNumbers = new List<int> ();
+		public List<string> teamQualities = new List<string> ();
+		public List<bool> yellowCoopStacks = new List<bool> ();
 
 		public int teamNumber;
 
@@ -58,24 +86,69 @@ namespace BearBones
 			Rest rest = new Rest ();
 			Task <ObservableCollection<ReportViewModel>> list =  rest.getReports (tNum);//SendAndReceiveJsonRequest ();
 			var reports = await list;
-			//var count = 0;
-			List<string> scores = new List<string> ();
-			List<string> drives = new List<string> ();
-			List<string> scouts = new List<string> ();
+			var count = 0;
+
+
 			foreach (ReportViewModel ip in reports) 
 			{
 				var p=ip;
-				//scoreEntry.Text = Convert.ToString (ip.score);
-				//driveEntry.Text = Convert.ToString (ip.drive);
-				//scoutEntry.Text = Convert.ToString (ip.scout);
 
-				scores.Add (ip.pointsScored);
-				drives.Add (ip.driveType);
-				scouts.Add (ip.scoutName);
+				pointsScoreds.Add (ip.pointsScored);
+				driveTypes.Add (ip.driveType);
+				scoutNames.Add (ip.scoutName);
+				autoCapabilities.Add(ip.autoCapability);
+				brokeDowns.Add(ip.brokeDown);
+				buildQualities.Add(ip.buildQuality);
+				grabsContainers.Add(ip.grabsContainer);
+				grabsContainerOffSteps.Add(ip.grabsContainerOffStep);
+				grabsTotes.Add(ip.grabsTote);
+				grabsToteOffSteps.Add(ip.grabsToteOffStep);
+				lastYearFinishes.Add(ip.lastYearFinish);
+				matchNumbers.Add(ip.matchNumber);
+				maxStacks.Add(ip.maxStack);
+				noodleBonuses.Add(ip.noodleBonus);
+				noodleCleanups.Add(ip.noodleCleanup);
+				noodleInContainers.Add(ip.noodleInContainer);
+				noteses.Add(ip.notes);
+				rebuildsStacks.Add(ip.rebuildsStack);
+				reportTypes.Add(ip.reportType);
+				setsContainerOnStacks.Add(ip.setsContainerOnStack);
+				stacksToteses.Add(ip.stacksTotes);
+				teamNames.Add(ip.teamName);
+				teamNumbers.Add(ip.teamNumber);
+				teamQualities.Add(ip.teamQuality);
+				yellowCoopStacks.Add(ip.yellowCoopStack);
 
 				InfoCell report = new InfoCell ();
 
-				this.Children.Add (report.CreatePage(ip.pointsScored, ip.driveType, ip.scoutName));
+				this.Children.Add (report.CreatePage(ip.pointsScored,
+					ip.driveType,
+					ip.scoutName,
+					ip.autoCapability,
+					ip.brokeDown,
+					ip.buildQuality,
+					ip.grabsContainer,
+					ip.grabsContainerOffStep,
+					ip.grabsTote,
+					ip.grabsToteOffStep,
+					ip.lastYearFinish,
+					ip.matchNumber,
+					ip.maxStack,
+					ip.noodleBonus,
+					ip.noodleCleanup,
+					ip.noodleInContainer,
+					ip.notes,
+					ip.rebuildsStack,
+					ip.reportType,
+					ip.setsContainerOnStack,
+					ip.stacksTotes,
+					ip.teamName,
+					ip.teamNumber,
+					ip.teamQuality,
+					ip.yellowCoopStack,
+					count));
+
+				//this.Children.Add (report.CreatePage(ip.pointsScored, ip.driveType, ip.scoutName));
 
 
 
@@ -84,9 +157,11 @@ namespace BearBones
 				//scouts [count] = Convert.ToString (ip.scout);
 				//scores.Add (Convert.ToString(ip.score));
 
-				//count++;
+				count++;
 
 			}
+
+			BuildGraphs ();
 
 
 		}
@@ -101,6 +176,58 @@ namespace BearBones
 		{
 			ScoutReportPage page = new ScoutReportPage (teamNumber.ToString());
 			await Navigation.PushModalAsync (page);
+		}
+
+		async void BuildGraphs()
+		{
+			Series firstBarSeries = new Series
+			{
+				Color = Color.Red,
+				Type = ChartType.Bar
+			};
+			firstBarSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 25});
+			firstBarSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 40});
+			firstBarSeries.Points.Add(new DataPoint() { Label = "March", Value = 45});
+
+			Series secondBarSeries = new Series
+			{
+				Color = Color.Blue,
+				Type = ChartType.Bar
+			};
+			secondBarSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 30 });
+			secondBarSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 35 });
+			secondBarSeries.Points.Add(new DataPoint() { Label = "March", Value = 40 });
+
+			Series lineSeries = new Series
+			{
+				Color = Color.Green,
+				Type = ChartType.Line
+			};
+			lineSeries.Points.Add(new DataPoint() { Label = "Jan",   Value = 27.5 });
+			lineSeries.Points.Add(new DataPoint() { Label = "Feb",   Value = 37.5 });
+			lineSeries.Points.Add(new DataPoint() { Label = "March", Value = 42.5 });
+
+
+			Chart chart = new Chart()
+			{
+				Color = Color.White,
+				WidthRequest = HomePage.ScreenWidth-10,
+				HeightRequest = 100,
+				Spacing = 10
+			};
+			chart.Series.Add(firstBarSeries);
+			chart.Series.Add(secondBarSeries);
+			chart.Series.Add(lineSeries);
+			StackLayout stack = new StackLayout ();
+
+			Label lbl = new Label {
+				Text = ""
+			};
+
+			graphs.Children.Add(lbl);
+			graphs.Children.Add(chart);
+
+
 		}
 
 	}
