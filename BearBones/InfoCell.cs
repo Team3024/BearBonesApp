a39968a8bfb3dbc74db100d18c52d21e97d4ec2a
MacOwner <>
2015-03-15 14:35:19 -0700

    Cleanup of unused data fields

--- BearBones/InfoCell.cs
+++ BearBones/InfoCell.cs
@@ -17,66 +17,24 @@ namespace BearBones
 
 		}
 
-		async void getPhoto(string name,Image iv)
-		{
-			//if (name == null)
-			//	return null;
-			//Task<ImageSource> result = DependencyService.Get<IAws> ().awsGetFile (name,null);
-			//return await result;
-			//ImageSource source = await result;
-			ImageSource isrc=null;
-			//DependencyService.Get<IAws> ().awsGetFile (name,null,iv);//source;
-			//return isrc;
-		}
 
-		public ContentPage CreatePage(string teamScore,
+		public ContentPage CreatePage(
+			int teamNumber,
+			string teamName,
+			string teamScore,
+			string matchNumber,
 			int toteScore,
 			int canScore,
-			string teamDrive,
-			string scoutName,
-			string autoCapabilities,
-			bool brokeDown,
-			string buildQuality,
-			bool grabsContainer,
-			bool grabsContainerOffStep,
-			bool grabsTote,
-			bool grabsToteOffStep,
-			string lastYearFinish,
-			string matchNumber,
-			string maxStack,
-			bool noodleBonus,
-			bool noodleCleaup,
-			bool noodleInContainer,
+			int noodleScore,
+			int coopScore,
 			string notes,
-			bool rebuildStack,
-			string reportType,
-			bool setsContainerOnStack,
-			bool stacksTotes,
-			string teamName,
-			int teamNumber,
-			string teamQuality,
-			bool yellowCoopStack,
+			bool brokeDown,
+			string autoCapabilities,
 			int currentPage,
 			int pageNumber)
 		{
 
-
-
 			string brokeDownText = (brokeDown) ? "broke down" : "didn\'t break down";
-			string grabsContainerText = (grabsContainer) ? "grabs containers" : "can\'t grab containers";
-			string grabsContainerOffStepText = (grabsContainerOffStep) ? "grabs containers off step" : "can\'t grab containers off step";
-			string grabsToteText = (grabsTote) ? "grabs totes" : "can\'t grab totes";
-			string grabsToteOffStepText = (grabsToteOffStep) ? "grabs tote off step" : "can\'t grab tote off step";
-			string noodleBonusText = (noodleBonus) ? "executes noodle bonus" : "can\'t execute noodle bonus";
-			string noodleCleanupText = (noodleCleaup) ? "cleans up noodles" : "can\'t clean up noodles";
-			string noodleInContainerText = (noodleInContainer) ? "puts noodle in container" : "can\'t put noodle in container";
-			string rebuildStackText = (rebuildStack) ? "rebuilds stacks" : "can\'t rebuild stacks";
-			string setsContainerOnStackText = (setsContainerOnStack) ? "sets containers on stacks" : "can\'t set containers on stacks";
-			string stacksTotesText = (stacksTotes) ? "stackes totes" : "can\'t stack totes";
-			string yellowCoopStackText = (yellowCoopStack) ? "execute co-op stack" : "can\'t execute co-op stack";
-
-		
-
 			StackLayout pageIndicator = new StackLayout{HorizontalOptions = LayoutOptions.CenterAndExpand, Orientation = StackOrientation.Horizontal};
 
 			for (int x = 0; x < pageNumber; x++) {
@@ -114,25 +72,8 @@ namespace BearBones
 							new Label {Text = "Total Individual Score: " + (toteScore + canScore).ToString(), TextColor = Color.White,
 								//FontSize=25,
 								FontAttributes= FontAttributes.Bold},
-							new Label {Text = "Drive: " + teamDrive,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
+							
 							new Label {Text = "Auto Capabilities: " + autoCapabilities,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							new Label {Text = "Build Quality: " + buildQuality,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							new Label {Text = "Team Quality: " + teamQuality,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							new Label {Text = "Max Stack: " + maxStack,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							new Label {Text = "Last Year Finish: " + lastYearFinish,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							new Label {Text = "Scout: " + scoutName,
 								//FontSize=25,
 								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
 							new Label {Text = "Match Number: " + matchNumber,
@@ -141,46 +82,7 @@ namespace BearBones
 							new Label {Text = "Notes: " + notes,
 								//FontSize=25,
 								FontAttributes= FontAttributes.Bold, TextColor = Color.White},
-							//Seperator
-							new BoxView{WidthRequest=HomePage.ScreenWidth, HeightRequest=2, Color=Color.Black, BackgroundColor=Color.Black},
-							//Robot Abilities
-							new Label {Text="Robot:", FontSize=20, FontAttributes = FontAttributes.Bold, TextColor = Color.Black},
-							new Label {Text = brokeDownText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = grabsContainerText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = grabsContainerOffStepText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = grabsToteText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = grabsToteOffStepText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = noodleBonusText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = noodleCleanupText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = noodleInContainerText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = rebuildStackText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = setsContainerOnStackText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = stacksTotesText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
-							new Label {Text = yellowCoopStackText, TextColor = Color.White,
-								//FontSize=25,
-								FontAttributes= FontAttributes.Bold},
+
 							pageIndicator
 						}
 
